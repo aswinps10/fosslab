f59e1306cf66bd8e620a53e18d99ec55bc559a16
aswinps10 <35956327+aswinps10@users.noreply.github.com>
2018-04-08 15:39:39 +0000

    commit

---
+++ numq2.sh
@@ -0,0 +1,10 @@
+echo "enter the three numbers"
+read a
+read b
+read c
+if [ "$a" -gt "$b" ] && [ "$a" -gt "$c" ]
+then echo $a is the greatest
+elif [ "$b" -gt "$a" ] && [ "$b" -gt "$c" ]
+then echo $b is the greatest
+else echo $c is the greatest
+fi
