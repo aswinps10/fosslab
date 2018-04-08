b645777aef97dfbef7e4f8f90099fd8d8af760c5
aswinps10 <35956327+aswinps10@users.noreply.github.com>
2018-04-08 15:45:20 +0000

    commit

---
+++ shellq4.sh
@@ -0,0 +1,20 @@
+#current logged in username
+echo $(logname)
+
+#current shell
+echo $SHELL
+
+#home directory
+echo $HOME
+
+#operating system type
+echo $(arch)
+
+#current path setting
+echo $PATH
+
+#present working directory
+echo %PWD
+
+#current logged in users
+echo %USERS
